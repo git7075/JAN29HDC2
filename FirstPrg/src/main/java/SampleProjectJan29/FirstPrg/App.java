af2a635b5e20f382d1b25747c7a1dcb3bd208104
ramesh.mukku <ramesh.mukku@HDC3-L-7190PL4.dir.svc.accenture.com>
2018-01-29 15:14:36 +0530

    commit JPrj

---
+++ FirstPrg/src/main/java/SampleProjectJan29/FirstPrg/App.java
@@ -0,0 +1,13 @@
+package SampleProjectJan29.FirstPrg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
